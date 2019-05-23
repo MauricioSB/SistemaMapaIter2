473ac6fcb7c83ba449f488ec9fa01870839fd18c
Mauricio Suárez Barrera <mausuarez@ciencias.unam.mx>
2019-05-23 09:11:55 -0500

    Caso de uso terminado sin probar

--- Simix/src/main/java/com/xiutech/simix/controlador/CalificarComentarioController.java
+++ Simix/src/main/java/com/xiutech/simix/controlador/CalificarComentarioController.java
@@ -11,9 +11,49 @@ package com.xiutech.simix.controlador;
  * @version 11/05/19 
  */
 public class CalificarComentarioController {
-    
-    public void calificaComentario(){
         
+    private int calificacion;
+    private Comentario coment;
+
+    public Comentario getComentario() {
+        return coment;
+    }
+
+    public void setComentario(Comentario coment) {
+        this.coment = coment;
+    }
+    
+    public int getCalificacion() {
+        return this.calificacion;
+    }
+    
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+    
+    public String calificaComentario(int id){
+        String mensaje = "Se califico el comentario exitosamente";
+        //busca al comentarista loggeado
+        ComentaristaDAO comentaristaUDB = new ComentaristaDAO();
+        SessionController.UserLogged usuario = (SessionController.UserLogged) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
+        Comentarista comentarista = comentaristaUDB.find(usuario.getCorreo());
+        //busca el marcador dado el id
+        MarcadorDAO marcadorUDB = new MarcadorDAO();
+        Marcador marcador = marcadorUDB.find(id);
+        //crea el id de la calificacion
+        CalificacionId calificacionId = new CalificacionId();
+        calificacionId.setIdMarcador(marcador.getIdMarcador());
+        calificacionId.setCorreoComentarista(comentarista.getCorreo());
+        //Se crea el comentario para agregarlo a la base de datos
+        Calificacion calif = new Calificacion();
+        calif.setCalificacion(getCalificacion()+1);
+        calif.setComentarista(comentarista);
+        calif.setComentario(coment);
+        calif.setId(calificacionId);        
+        //Se agrega a la base de datos.
+        CalificacionDAO calificacionUDB = new CalificacionDAO ();
+        calificacionUDB.save(calif);        
+        return "/general/MensajeExitoIH?faces-redirect=true&mensaje=" + mensaje;
     }
     
 }
